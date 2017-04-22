b277a69023afce4ea49e2411f11a7a2740b170db
PolinaEremeeva <polundrek@mail.ru>
2017-04-22 17:20:52 +0300

    test

--- src/main/java/action/admin/resumes/CreateResumeAdminPanelAction.java
+++ src/main/java/action/admin/resumes/CreateResumeAdminPanelAction.java
@@ -3,6 +3,8 @@ package action.admin.resumes;
 import action.interceptor.AuthorizeAs;
 import action.resumes.CreateResumeAction;
 
+/*test*/
+
 @AuthorizeAs(admin = true)
 public class CreateResumeAdminPanelAction extends CreateResumeAction {
 }
